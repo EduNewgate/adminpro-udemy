66c9b9ee64abe14c48d849b2e6ca64731e4b6eef
EduNewgate <eduardo.moreno.93@gmail.com>
2020-07-11 16:43:12 +0200

    Modificaciones de módulos y rutas.

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphics1Component } from './graphics1/graphics1.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'pregress', component: ProgressComponent },
+      { path: 'graphics1', component: Graphics1Component }
+    ],
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+
+export class PagesRoutingModule {}
